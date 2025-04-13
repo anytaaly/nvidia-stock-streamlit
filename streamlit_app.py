6398c7450cbc5443376a4333e41a6a0f56f63de2
Anyta Ali <anytarali@gmail.com>
2025-04-13 15:14:55 +0000

    added 10 years of data

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ st.title("\U0001F4C8 NVIDIA (NVDA) Stock Price Viewer")
 # ----------------------
 # Sidebar Inputs
 # ----------------------
-period = st.sidebar.selectbox("Select Time Period", ["1mo", "3mo", "6mo", "1y", "2y", "5y"], index=5)
+period = st.sidebar.selectbox("Select Time Period", ["1mo", "3mo", "6mo", "1y", "2y", "5y", "10y"], index=5)
 interval = st.sidebar.selectbox("Select Interval", ["1d", "1wk", "1mo"], index=0)
 refresh_rate = 300  # Set to refresh every 5 minutes (300 seconds)
 
